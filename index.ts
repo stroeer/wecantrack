55ff09f584a2bf6034caa0eeea6122a829030c64
Jan Meischner <jan.meischner@stroeer.de>
2021-09-30 13:02:35 +0200

    ✨ expose more types

--- index.ts
+++ index.ts
@@ -1,5 +1,9 @@
 export { WeCanTrack } from './src/api';
 export { ClickoutRequestBuilder } from './src/clickout';
 export type { ClickoutData, ClickoutResponse } from './src/clickout';
+export { NetworkAccountRequestBuilder } from './src/network-accounts';
+export type { NetworkAccount } from './src/network-accounts';
+export type { Network } from './src/networks';
 export { TransactionRequestBuilder } from './src/transactions';
 export type { TransactionData, TransactionResponse } from './src/transactions';
+export type { DateType, WCTDate } from './src/types';
